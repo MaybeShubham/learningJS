9917e988cfbd20f186faa6adb0f5fa237088960f
Shubham Pathare <shubhampathare.2006@gmail.com>
2024-01-15 02:27:20 +0530

    Server
    I learned how to access a port and host a server

---
+++ Day 1/test.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const port = 80;
+
+const sever = http.createServer(function (req, res) {
+  res.write("hello world");
+  res.end();
+});
+
+sever.listen(port, function (error) {
+  if (error) {
+    console.log("something went wrong", error);
+  } else {
+    console.log("server is listening on port" + port);
+  }
+});
