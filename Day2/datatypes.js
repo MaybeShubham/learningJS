28fff598da0a6757b3ce1c945c9c67d2b5d2487c
Shubham Pathare <shubhampathare.2006@gmail.com>
2024-01-16 00:16:11 +0530

    done with datatypes

---
+++ Day2/datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version of js
+
+//alert(3 + 3);  //we are using node.js not browser
+
+console.log(3 + 3); //code readability should be high
+
+console.log("shubham");
+
+let name = "Shubham";
+let age = 17;
+let isLoggedIn = false;
+let state;
+
+/*number => 2^53
+bigint 
+string => ""
+booloean => true/false 
+null => standalone value
+undefined => value not defined
+symbol => unique 
+*/
+
+// object
+
+console.log(typeof undefined);
+console.log(typeof null);
