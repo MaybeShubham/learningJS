204fb186d764259da0eda87de1806a0f65bc32a9
Shubham Pathare <shubhampathare.2006@gmail.com>
2024-01-30 12:47:06 +0530

    datatype conversion code
    learned about data type confusion caused in javascript

---
+++ Day3/conversionoperation.js
@@ -0,0 +1,24 @@
+let score = 69;
+
+console.log(typeof score);
+
+let valuInNumber = Number(score);
+console.log(valuInNumber);
+
+// "33" = 33
+// "33abc" = Nan
+// true = 1 ; false = 0
+
+let isLoggedIn = "shubham";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// ` = true ; 0 = false
+// "" = false
+// "shubham" = true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
